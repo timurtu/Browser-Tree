1e80417e4145d21bb61585e72c6930cadd1fb48e
Timur Uzel <timurt.uzel@gmail.com>
2016-04-17 21:07:14 -0400

    this is a desktop app there should be no pointers

--- render/sidebar.js
+++ render/sidebar.js
@@ -6,7 +6,9 @@ const menuButton = document.getElementById('menu')
 const newtabButton = document.getElementById('newtab')
 const sidebar = document.getElementById('sidebar')
 const views = document.getElementById('views')
-const tabs = document.getElementById('tabs').childNodes
+const tabList = document.getElementById('tabs')
+
+let homepage
 
 menuButton.onclick = () => {
   toggleSidebar()
@@ -39,5 +41,23 @@ function toggleSidebar() {
  * in the sidebar.
  */
 function createTab() {
-  console.log(tabs);
+  // console.log(tabs)
+
+  homepage = `https://google.com`;
+
+  // Remove active tab
+  Array.prototype.forEach.call(tabList.childNodes, (tab) => {
+    if(tab.classList.contains('active')) {
+      tab.classList.remove('active')
+    }
+  })
+
+  // Create new tab
+  const newTab = document.createElement('a')
+  newTab.className = 'tab list-group-item active'
+  newTab.role = 'button'
+  newTab.textContent = homepage
+
+  tabList.appendChild(newTab)
+
 }
