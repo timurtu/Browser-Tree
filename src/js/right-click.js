7496bcfbbc65a16c3e70e6ef2b71a80f972aeea8
Timur Uzel <timurt.uzel@gmail.com>
2016-04-24 14:17:29 -0400

    simple right click menu

---
+++ src/js/right-click.js
@@ -0,0 +1,38 @@
+/**
+ * Created by timur on 4/24/16.
+ */
+
+import {remote} from 'electron'
+
+const Menu = remote.Menu
+const MenuItem = remote.MenuItem
+let menu = new Menu()
+let rightClickMenuItems = [
+  {
+    label: 'Back',
+    click: event => {
+      back()
+    }
+  },
+  {
+    label: 'Forward',
+    click: event => {
+      forward()
+    }
+  },
+  {
+    type: 'separator'
+  }
+]
+
+rightClickMenuItems.forEach(menuItem => {
+  menu.append(new MenuItem(menuItem))
+})
+
+window.addEventListener('contextmenu', (event) => {
+
+  event.preventDefault()
+
+  menu.popup(remote.getCurrentWindow())
+
+}, false)
